f8d5b0abf2d250dd50c827c0536111aa4029aeca
Makor <mateusz.budzar@droidsonroids.pl>
2019-02-27 07:27:41 +0100

    Add consenta validation logic to consents view

--- app/src/main/java/com/makor/compoundviewexample/ConsentsView.kt
+++ app/src/main/java/com/makor/compoundviewexample/ConsentsView.kt
@@ -4,12 +4,26 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.consents_view.view.*
 
 class ConsentsView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
+    var onCheckedChangeListener: OnConsentsCheckedChangeListener? = null
+
     init {
         LayoutInflater.from(context).inflate(R.layout.consents_view, this, true)
+
+        consent1.setOnCheckedChangeListener { _, _ -> validateConsents() }
+        consent2.setOnCheckedChangeListener { _, _ -> validateConsents() }
     }
+
+    private fun validateConsents() {
+        onCheckedChangeListener?.onCheckedChange(consent1.isChecked && consent2.isChecked)
+    }
+}
+
+interface OnConsentsCheckedChangeListener {
+    fun onCheckedChange(allConsentsChecked: Boolean)
 }
--- app/src/main/java/com/makor/compoundviewexample/MainActivity.kt
+++ app/src/main/java/com/makor/compoundviewexample/MainActivity.kt
@@ -2,11 +2,18 @@ package com.makor.compoundviewexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        consentsView.onCheckedChangeListener = object: OnConsentsCheckedChangeListener {
+            override fun onCheckedChange(allConsentsChecked: Boolean) {
+                confirmButton.isEnabled = allConsentsChecked
+            }
+        }
     }
 }
