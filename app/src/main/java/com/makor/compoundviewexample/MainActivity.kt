2ee0b2803688bfec489be5c81fbb90d1f2c9f827
Makor <mateusz.budzar@droidsonroids.pl>
2019-02-27 07:10:24 +0100

    Enalbe confirm button when consents are checked

--- app/src/main/java/com/makor/compoundviewexample/MainActivity.kt
+++ app/src/main/java/com/makor/compoundviewexample/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.makor.compoundviewexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        consent1.setOnCheckedChangeListener { _, _ -> validateConsents() }
+        consent2.setOnCheckedChangeListener { _, _ -> validateConsents() }
+    }
+
+    private fun validateConsents() {
+        confirmButton.isEnabled = consent1.isChecked && consent2.isChecked
     }
 }
