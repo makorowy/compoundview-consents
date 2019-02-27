9d90647bab06072e8f282e92315baa82789d10ba
Makor <mateusz.budzar@droidsonroids.pl>
2019-02-27 19:48:20 +0100

    Add custom attribute

--- app/src/main/java/com/makor/compoundviewexample/ConsentsView.kt
+++ app/src/main/java/com/makor/compoundviewexample/ConsentsView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.consents_view.view.*
 
@@ -21,6 +22,12 @@ class ConsentsView @JvmOverloads constructor(
 
         consent1.setOnCheckedChangeListener { _, _ -> validateConsents() }
         consent2.setOnCheckedChangeListener { _, _ -> validateConsents() }
+
+        context.theme.obtainStyledAttributes(attrs, R.styleable.ConsentsView, 0, 0).apply {
+            val separatorVisible = getBoolean(R.styleable.ConsentsView_separatorVisibility, true)
+            consentsSeparator.visibility = if (separatorVisible) View.VISIBLE else View.GONE
+            recycle()
+        }
     }
 
     private fun validateConsents() {
